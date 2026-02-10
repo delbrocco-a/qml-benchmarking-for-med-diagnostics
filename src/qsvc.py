8b56361d643b8f9c7b953c12c595755b3cc2c3b3
ellisdeibroncci <alexander.delbrocco@proton.me>
2026-02-10 12:21:05 +0000

    test: qsvc test module
    - Includes other minor refactoring changes

--- src/qsvc.py
+++ src/qsvc.py
@@ -4,7 +4,7 @@ from qiskit_machine_learning.kernels import FidelityQuantumKernel
 from src.load_data import TRAINING, TESTING, FEATURES, TARGETS
 
 def trainQSVC(qkernel: FidelityQuantumKernel, data: dict) -> QSVC:
-  """Trains a QSVC model using prepared quantum kernel & data"""
+  """Trains a QSVC model using prepared quantum kernel & training data"""
   
   qsvc = QSVC(kernel=qkernel)
   qsvc.fit(data[FEATURES], data[TARGETS])
@@ -12,6 +12,7 @@ def trainQSVC(qkernel: FidelityQuantumKernel, data: dict) -> QSVC:
   return qsvc
 
 
-def testQSVC(qsvc: QSVC, data: dict) -> float:
-  """Tests a QSVC model using prepared data, returns accuracy score"""
+def evalQSVC(qsvc: QSVC, data: dict) -> float:
+  """Tests a QSVC model using testing data, returns accuracy score"""
+
   return qsvc.score(data[FEATURES], data[TARGETS])
