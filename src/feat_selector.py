920688a89a6d5810b47960bc0abbc6bbe73badbd
ellisdeibroncci <alexander.delbrocco@proton.me>
2026-02-10 06:49:26 +0000

    2026-02-09-EoS

---
+++ src/feat_selector.py
@@ -0,0 +1,13 @@
+from sklearn.datasets import load_iris
+from sklearn.feature_selection import SelectKBest
+from sklearn.feature_selection import f_classif
+
+## Recursive Feature Elimination (RFE) for feature selection
+
+## SelectFromModel Feature Selection
+
+## L1-Based Feature Selection
+
+## Tree-Based Feature Selection
+
+## SelectKBest Feature Selection
--- src/q_econde.py
+++ src/q_econde.py
@@ -14,3 +14,7 @@ from qiskit_machine_learning.kernels import FidelityQuantumKernel
 kernel = FidelityQuantumKernel(feature_map=feature_map)
 
 # Featy
+
+def generate_feature_map(feature_dimension: int, reps: int) -> ZZFeatureMap:
+  """Generates a ZZFeatureMap for given feature dimension and repetitions."""
+  return ZZFeatureMap(feature_dimension=feature_dimension, reps=reps)
