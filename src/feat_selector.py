17d04bc2fdb2760c00478fc84fa6b5b2ca0fd4b2
ellisdeibroncci <alexander.delbrocco@proton.me>
2026-02-10 08:19:10 +0000

    feat: pca feature selection

--- src/feat_selector.py
+++ src/feat_selector.py
@@ -1,13 +1,28 @@
-from sklearn.datasets import load_iris
-from sklearn.feature_selection import SelectKBest
-from sklearn.feature_selection import f_classif
+from sklearn.decomposition import PCA
+from sklearn.feature_selection import SelectKBest, f_classif
 
-## Recursive Feature Elimination (RFE) for feature selection
+import numpy as np
 
-## SelectFromModel Feature Selection
+# Classical PCA (reduce to k features)
+pca = PCA(n_components=4)  # Keep 4 features
+X_reduced = pca.fit_transform(X)
 
-## L1-Based Feature Selection
+# OR SelectKBest (keep top k features)
+selector = SelectKBest(f_classif, k=4)
+X_selected = selector.fit_transform(X, y)
 
-## Tree-Based Feature Selection
-
-## SelectKBest Feature Selection
+def PCASelect(
+  select: int, feats: int, data: dict
+) -> tuple[np.ndarray, np.ndarray]:
+  
+  selector = SelectKBest(f_classif, k=select)
+  feats_train_select = selector.fit_transform(
+    data["training"]["features"], data["training"]["targets"]
+  )
+  feats_test_select = selector.transform(data["testing"]["features"]) 
+  
+  pca = PCA(n_components=feats)
+  feats_train_pca = pca.fit_transform(feats_train_select)
+  feats_test_pca = pca.transform(feats_test_select)
+  
+  return feats_train_pca, feats_test_pca
