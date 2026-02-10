6188bfc8b2b9563098ba3708f746d471f06cbd66
ellisdeibroncci <alexander.delbrocco@proton.me>
2026-02-10 07:20:14 +0000

    feat: Quantum Kernel Initialiser

---
+++ src/qkernel.py
@@ -0,0 +1,20 @@
+from qiskit.circuit.library import ZZFeatureMap, ZFeatureMap, PauliFeatureMap
+from qiskit_machine_learning.kernels import FidelityQuantumKernel
+from typing import Optional
+
+ZZ    = "ZZFeatureMap"
+Z     = "ZFeatureMap"
+PAULI = "PauliFeatureMap"
+
+def FQKernel(
+  qubits: int, encodes: int, map: Optional[str]="ZZFeatureMap"
+) -> FidelityQuantumKernel:
+  """Generates a Quantum Kernel using the ZZ feature map"""
+  match(map):
+    case "ZFeatureMap":
+      feat_map = ZFeatureMap(feature_dimension=qubits, reps=encodes)
+    case "PauliFeatureMap":
+      feat_map = PauliFeatureMap(feature_dimension=qubits, reps=encodes)
+    case _:
+      feat_map = ZZFeatureMap(feature_dimension=qubits, reps=encodes)
+  return FidelityQuantumKernel(feature_map=feat_map)
