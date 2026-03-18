db65b124274520584f1453e0931ad8dc72ca64c6
ellisdeibroncci <alexander.delbrocco@proton.me>
2026-03-18 09:55:24 +0000

    improved setup.sh

--- setup/setup.sh
+++ setup/setup.sh
@@ -1,3 +1,18 @@
 # Assuming you have already executed 'conda init;'
-conda env create -f qiskit-env.yml;
-conda activate qiskit-env;
+#!/bin/bash
+set -e
+
+echo "Creating qml-env..."
+conda env create -f qiskit-env.yml
+
+echo "Activating qml-env..."
+source ~/.bashrc
+conda activate qml-env
+
+echo "Installing Qiskit packages..."
+pip install qiskit qiskit-machine-learning
+
+echo "Installing QuPCA..."
+pip install git+https://github.com/Eagle-quantum/QuPCA.git
+
+echo "Done. Run 'conda activate qml-env' to start."
