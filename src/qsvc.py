5bfd360d4b981795d0538093320546142dcba2ff
ellisdeibroncci <alexander.delbrocco@proton.me>
2026-03-18 09:36:18 +0000

    Updated environment

--- src/qsvc.py
+++ src/qsvc.py
@@ -1,4 +1,5 @@
 from qiskit_machine_learning.algorithms.classifiers import QSVC
+from qiskit_machine_learning.algorithms import PegasosQSVC
 from qiskit_machine_learning.kernels import FidelityQuantumKernel
 
 from src.load_data import TRAINING, TESTING, FEATURES, TARGETS
@@ -17,3 +18,20 @@ def evalQSVC(qsvc: QSVC, data: dict) -> float:
   """Tests a QSVC model using testing data, returns accuracy score"""
 
   return qsvc.score(data[FEATURES], data[TARGETS])
+
+
+def trainPQSVC(
+  qkernel: FidelityQuantumKernel, data: dict, C: float, tau: int
+) -> PegasosQSVC:
+  """Trains a PegasosQSVC model using prepared quantum kernel & data"""
+  
+  pegasos_qsvc = PegasosQSVC(quantum_kernel=qkernel, C=C, num_steps=tau)
+  pegasos_qsvc.fit(data[FEATURES], data[TARGETS])
+
+  return pegasos_qsvc
+
+
+def evalPQSVC(pegasos_qsvc: PegasosQSVC, data: dict) -> float:
+  """Tests a PegasosQSVC model using testing data, returns accuracy score"""
+
+  return pegasos_qsvc.score(data[FEATURES], data[TARGETS])
